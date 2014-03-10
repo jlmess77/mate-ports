812159d4a35aefd47e50b34e042dc88dff9c2a03
ericbsd <ericturgeon.bsd@gmail.com>
2014-03-09 22:32:46 -0300

    Commit some change for mate 1.8

---
+++ x11/mate-screensaver/work/mate-screensaver-1.6.1/config.h
@@ -0,0 +1,216 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* always defined to indicate that i18n is enabled */
+#define ENABLE_NLS 1
+
+/* Name of default gettext domain */
+#define GETTEXT_PACKAGE "mate-screensaver"
+
+/* Define if system uses adjunct shadow passwords */
+/* #undef HAVE_ADJUNCT_PASSWD */
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* Define to 1 if you have the `clearenv' function. */
+/* #undef HAVE_CLEARENV */
+
+/* Define to 1 if you have the <crypt.h> header file. */
+/* #undef HAVE_CRYPT_H */
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if system uses enhanced shadow passwords */
+/* #undef HAVE_ENHANCED_PASSWD */
+
+/* Define to 1 if you have the `fcntl' function. */
+#define HAVE_FCNTL 1
+
+/* Define to 1 if you have the `getcwd' function. */
+#define HAVE_GETCWD 1
+
+/* Define to 1 if you have the `getresuid' function. */
+#define HAVE_GETRESUID 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define to 1 if you have the `getwd' function. */
+#define HAVE_GETWD 1
+
+/* Define if system uses HPUX shadow passwords */
+/* #undef HAVE_HPUX_PASSWD */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define if libgl is available */
+#define HAVE_LIBGL 1
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if the MIT screensaver extension is available */
+#define HAVE_MIT_SAVER_EXTENSION 1
+
+/* Define to 1 if you have the `nice' function. */
+#define HAVE_NICE 1
+
+/* Define if PAM support is included */
+#define HAVE_PAM 1
+
+/* Define to 1 if you have the `putenv' function. */
+#define HAVE_PUTENV 1
+
+/* Define to 1 if you have the `realpath' function. */
+#define HAVE_REALPATH 1
+
+/* Define to 1 if you have the `sbrk' function. */
+#define HAVE_SBRK 1
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have the `setenv' function. */
+#define HAVE_SETENV 1
+
+/* Define to 1 if you have the `setpriority' function. */
+#define HAVE_SETPRIORITY 1
+
+/* Define to 1 if you have the `setresuid' function. */
+#define HAVE_SETRESUID 1
+
+/* Define to 1 if you have the `setrlimit' function. */
+#define HAVE_SETRLIMIT 1
+
+/* Define if system uses traditional shadow passwords */
+/* #undef HAVE_SHADOW_PASSWD */
+
+/* Define if shape extension is available */
+#define HAVE_SHAPE_EXT 1
+
+/* Define to 1 if you have the `sigaction' function. */
+#define HAVE_SIGACTION 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `syslog' function. */
+#define HAVE_SYSLOG 1
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the `uname' function. */
+#define HAVE_UNAME 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `unsetenv' function. */
+#define HAVE_UNSETENV 1
+
+/* Define this if you have the XF86MiscSetGrabKeysState function */
+#define HAVE_XF86MISCSETGRABKEYSSTATE /**/
+
+/* Define if XF86VMODE Gamma is available */
+#define HAVE_XF86VMODE_GAMMA 1
+
+/* Define if XF86VMODE Gamma Ramp is available */
+#define HAVE_XF86VMODE_GAMMA_RAMP 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define if screen locking support is disabled */
+/* #undef NO_LOCKING */
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "mate-screensaver"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://www.mate-desktop.org/"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "mate-screensaver"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "mate-screensaver 1.6.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "mate-screensaver"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.6.1"
+
+/* Define if pam_strerror takes two arguments */
+#define PAM_STRERROR_TWO_ARGS 1
+
+/* Full pathname of password helper application */
+#define PASSWD_HELPER_PROGRAM "/usr/local/bin/pam_helper"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.6.1"
+
+/* ConsoleKit support */
+#define WITH_CONSOLE_KIT 1
+
+/* Define if keyboard layout indicator should be built */
+#define WITH_KBD_LAYOUT_INDICATOR 1
+
+/* Define for libnotify support */
+#define WITH_LIBNOTIFY 1
+
+/* systemd support */
+/* #undef WITH_SYSTEMD */
+
+/* Define to the directory containing XScreensaver configuration files */
+#define XSCREENSAVER_CONFIG_DIR "/usr/local/share/xscreensaver/config"
+
+/* Define to the directory containing XScreensaver hack executables */
+#define XSCREENSAVER_HACK_DIR "/usr/local/bin/xscreensaver-hacks"
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
---
+++ x11/mate-screensaver/work/mate-screensaver-1.6.1/savers/gs-theme-engine-marshal.h
@@ -0,0 +1,20 @@
+
+#ifndef __gs_theme_engine_marshal_MARSHAL_H__
+#define __gs_theme_engine_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* BOOLEAN:BOXED (gs-theme-engine-marshal.list:1) */
+extern void gs_theme_engine_marshal_BOOLEAN__BOXED (GClosure     *closure,
+                                                    GValue       *return_value,
+                                                    guint         n_param_values,
+                                                    const GValue *param_values,
+                                                    gpointer      invocation_hint,
+                                                    gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* __gs_theme_engine_marshal_MARSHAL_H__ */
+
